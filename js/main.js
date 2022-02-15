83b5148485582fce7433384bc58a7d4d6ef7657b
Saloxiddin <abduraxmon.69list.ru@gmail.com>
2022-02-15 16:52:51 +0500

    edit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+window.addEventListener('load', ()=> {
+    const body = document.querySelector('body')
+    const loadWrap = document.querySelector('.loader-wrapper')
+
+    setTimeout(function () {
+        body.classList.add('loaded')
+        loadWrap.classList.add('no-js')
+    })
+})
+
 AOS.init();
 navbar = document.querySelector(".nav-item").querySelectorAll(".nav-link");
 navbar.forEach(element => {
@@ -19,3 +29,26 @@ function mobileMenu() {
     burger.classList.toggle("active");
     navItem.classList.toggle("active");
 }
+
+const bot = new Bot('5151400270:AAEjzlJ1kFzCBT4FRCfMrXai9lpGz63xnac', 1393665338)
+
+document.querySelector('#form').addEventListener('submit', (e)=> {
+    e.preventDefault()
+
+    const text = document.querySelector('.inName').value
+    const text1 = document.querySelector('.email').value
+    const text2 = document.querySelector('.last').value
+    const text3 = document.querySelector('.textarea').value
+    bot.sendMessage(text).then(res => {
+        console.log(res)
+    })
+    bot.sendMessage(text1).then(res => {
+        console.log(res)
+    })
+    bot.sendMessage(text2).then(res => {
+        console.log(res)
+    })
+    bot.sendMessage(text3).then(res => {
+        console.log(res)
+    })
+})
